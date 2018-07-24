98a4b3f66d53ea41349d6c134e433ca2aada51dd
mattb20 <matt_burgoyne@live.co.uk>
2018-07-24 14:09:54 +0100

    hello world in csharp

---
+++ helloworld/helloworld/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace helloworld
+{
+    class Hello
+    {
+        static void Main() 
+        {
+            Console.WriteLine("Hello World!");
+
+            //Keep the console window open in debug mode
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
